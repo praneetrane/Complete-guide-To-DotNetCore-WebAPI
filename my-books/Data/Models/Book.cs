ee90306b935ba765452b08a4ea60c0e3d9fab2e0
Praneet Rane <praneet.rane@gmail.com>
2021-04-27 20:40:42 +0530

    One to Many Relationship in Entity Framework Core

--- my-books/Data/Models/Book.cs
+++ my-books/Data/Models/Book.cs
@@ -23,5 +23,9 @@ namespace my_books.Data.Models
         public string Author { get; set; }
         public string CoverUrl { get; set; }
         public DateTime DateAdded { get; set; }
+
+        //Navigation Properties
+        public int PublisherID { get; set; }
+        public Publisher Publisher { get; set; }
     }
 }
