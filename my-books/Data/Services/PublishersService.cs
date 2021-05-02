73874485f5803d9a857dbdc72cf9aefd91b4efa4
Praneet Rane <praneet.rane@gmail.com>
2021-05-02 16:40:49 +0530

    Getting Books of a Publisher including Authors

--- my-books/Data/Services/PublishersService.cs
+++ my-books/Data/Services/PublishersService.cs
@@ -21,11 +21,26 @@ namespace my_books.Data.Services
             var _publisher = new Publisher()
             {
                 Name = publisher.Name
-                
+
             };
             _context.Publishers.Add(_publisher);
             _context.SaveChanges();
         }
 
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Title,
+                        BookAuthors = n.Book_Authors.Select(n => n.Author.FullName).ToList()
+                    }).ToList()
+                }).FirstOrDefault();
+            return _publisherData;
+        }
     }
 }
