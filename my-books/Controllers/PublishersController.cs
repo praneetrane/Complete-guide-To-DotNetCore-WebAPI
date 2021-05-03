b797014736768ca6389c4cd770a809d4f91525a2
Praneet Rane <praneet.rane@gmail.com>
2021-05-03 22:35:41 +0530

    HTTP Response Status Codes

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -23,8 +23,24 @@ namespace my_books.Controllers
         [HttpPost("add-publisher")]
         public IActionResult AddAuthor([FromBody] PublisherVM publisher)
         {
-            _publishersService.AddPublisher(publisher);
-            return Ok();
+            var newPublisher = _publishersService.AddPublisher(publisher);
+            return Created(nameof(AddAuthor),newPublisher);
+        }
+
+        [HttpGet("get-publisher-by-id/{id}")]
+
+        public IActionResult GetPublisherById(int id)
+        {
+            var _response = _publishersService.GetPublisherById(id);
+            if (_response != null)
+            {
+                return Ok(_response);
+            }
+            else
+            {
+                return NotFound();
+            }
+           
         }
 
         [HttpGet("get-publisher-books-with-authors/{id}")]
@@ -36,7 +52,7 @@ namespace my_books.Controllers
         }
 
 
-        [HttpDelete ("delete-publisher-by-id/{id}")]
+        [HttpDelete("delete-publisher-by-id/{id}")]
 
         public IActionResult DeletePublisherById(int id)
         {
--- my-books/Data/Services/PublishersService.cs
+++ my-books/Data/Services/PublishersService.cs
@@ -16,7 +16,7 @@ namespace my_books.Data.Services
 
         }
 
-        public void AddPublisher(PublisherVM publisher)
+        public Publisher AddPublisher(PublisherVM publisher)
         {
             var _publisher = new Publisher()
             {
@@ -25,8 +25,12 @@ namespace my_books.Data.Services
             };
             _context.Publishers.Add(_publisher);
             _context.SaveChanges();
+
+            return _publisher;
         }
 
+        public Publisher GetPublisherById(int id) => _context.Publishers.FirstOrDefault(n => n.Id == id);
+        
 
         public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
         {
